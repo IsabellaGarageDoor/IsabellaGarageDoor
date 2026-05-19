c9d74099415f07f5cc8542270ad2ac25e0a0f452
IsabellaGarageDoor <petertschwab@gmail.com>
2026-05-19 05:00:41 -0400

    messed with astro config some more

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,20 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import alpinejs from '@astrojs/alpinejs';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://isabellagaragedoor.com',
-  output: 'static', 
+  output: 'static',
+  legacy: {
+    collections: true,
+  },
   integrations: [tailwind(), sitemap(), react(), alpinejs()],
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes
       theme: 'github-light',
     },
     remarkPlugins: [],
     rehypePlugins: [],
-    // This setting allows proper encoding for extended characters
     smartypants: false,
   },
 });
