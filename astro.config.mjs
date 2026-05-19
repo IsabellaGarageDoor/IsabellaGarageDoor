6a3cc38f9c883e74c152880e071021eda723d87d
IsabellaGarageDoor <petertschwab@gmail.com>
2026-05-19 05:14:49 -0400

    went with web3forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,21 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import alpinejs from '@astrojs/alpinejs';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://isabellagaragedoor.com',
-  output: 'static',
-  legacy: {
-    collections: true,
-  },
+  output: 'server', // Keep this for development
+  adapter: netlify(), 
   integrations: [tailwind(), sitemap(), react(), alpinejs()],
   markdown: {
     shikiConfig: {
+      // Choose from Shiki's built-in themes
       theme: 'github-light',
     },
     remarkPlugins: [],
     rehypePlugins: [],
+    // This setting allows proper encoding for extended characters
     smartypants: false,
   },
 });
